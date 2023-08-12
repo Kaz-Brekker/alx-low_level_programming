ce4a790cbb38d92ca73be950983e08464818f8ee
Kaz-Brekker <ermiaswoldekidan@gmail.com>
2023-08-12 04:46:32 -0700

    pls work

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - void
+ * Return: 0 success
+ *
+ * just printing size
+ */
+int main(void)
+{
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+	printf("Size of long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of float: %lu bytes\n", sizeof(float));
+}
