fcba06daf5facc9bad54d63c2b3d660bfed8df72
Kaz-Brekker <ermiaswoldekidan@gmail.com>
2023-08-12 06:37:13 -0700

    a return with 1

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - (void)
+ * Return: 1 successfully unseccessful
+ *
+ */
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
