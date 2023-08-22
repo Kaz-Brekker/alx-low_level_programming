780d148eddb7f897833d6ec84e87df373b9e1995
Kaz-Brekker <ermiaswoldekidan@gmail.com>
2023-08-21 19:40:32 -0700

    not ur friend

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * puts_half - half of a string
+ * 
+ * @str: input
+ * Return: half of input
+ */
+void puts_half(char *str)
+{
+	int a, n, lon;
+
+	lon = 0;
+
+	for (a = 0; str[a] != '\0'; a++)
+		lon++;
+
+	n = (lon / 2);
+
+	if ((lon % 2) == 1)
+		n = ((lon + 1) / 2);
+
+	for (a = n; str[a] != '\0'; a++)
+		_putchar(str[a]);
+	_putchar('\n');
+}
